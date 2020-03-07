774dc4b5703b7937836c921be882f73e2ffaecd7
root <708881841@qq.com>
2020-03-07 17:37:40 +0800

    5001

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def items():
 if __name__ == '__main__':
     app.config["JSON_AS_ASCII"] = False
     #app.run(debug=True,host= '0.0.0.0',port=5000)
-    WSGIServer(('0.0.0.0', 5000), app).serve_forever()
+    WSGIServer(('0.0.0.0', 5001), app).serve_forever()
